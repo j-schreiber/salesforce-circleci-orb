1e9b27747085b2833204fd92b5e163ee3f4784df
Jannis Schreiber <info@lietzau-consulting.de>
2022-02-27 17:04:54 +0100

    [semver:minor] first dev release (#1)
    * add circleci-orb-demo-package as submodule
    
    * chore: default github templates
    
    * chore: initial boilerplate cleanup
    
    * feat: install command
    
    * ops: add install step to pipeline
    
    * ops: add packaging commands job to pipeline
    
    * feat: add auth command
    
    * ops: add auth command to pipeline

---
+++ src/scripts/auth.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+set -e
+
+verify_params() {
+    if [ -z "${PARAM_JWT_KEY_FILE}" ]; then
+        echo "The JWT key is required. Please ensure the <<parameters.jwtKey>> env var has been set correctly or use the default value: SFDX_JWT_KEY."
+        exit 1
+    fi
+    if [ -z "${!PARAM_JWT_KEY_FILE}" ]; then
+        echo "The variable ${PARAM_JWT_KEY_FILE} is empty. Please store a valid base64 encoded JWT-key file"
+        exit 1
+    fi
+    if [ -z "${PARAM_CLIENT_ID}" ]; then
+        echo "A consumer key is required. Please ensure the <<parameters.consumerKey>> env var has been set correctly or use the default value: SFDX_CONSUMER_KEY"
+        exit 1
+    fi
+    if [ -z "${!PARAM_CLIENT_ID}" ]; then
+        echo "The variable ${PARAM_CLIENT_ID} is empty. Please store a valid consumer key for the connected app."
+        exit 1
+    fi
+    if [ -z "$PARAM_USERNAME" ]; then
+        echo "Username is empty. Set a username that is pre-authorized with the connected app"
+        exit 1
+    fi
+}
+
+decrypt_key() {
+    echo "${!PARAM_JWT_KEY_FILE}" | base64 --decode --ignore-garbage > ./server.key
+}
+
+sfdx_auth_jwt_grant() {
+    sfdx auth:jwt:grant "$@"
+}
+
+authenticate_org() {
+    if [[ -z "$PARAM_INSTANCE_URL" ]]; then
+        export PARAM_INSTANCE_URL=https://login.salesforce.com
+    fi
+    echo "Authorizing $PARAM_USERNAME at $PARAM_INSTANCE_URL ..."
+    params=()
+    params+=(--clientid "${!PARAM_CLIENT_ID}")
+    params+=( --jwtkeyfile server.key)
+    params+=( --username "$PARAM_USERNAME")
+    params+=( --instanceurl "$PARAM_INSTANCE_URL")
+    if [ "$PARAM_SET_DEFAULT" = true ] || [ "$PARAM_SET_DEFAULT" -eq 1 ]; then
+        echo "Setting org as default devhub ..."
+        params+=( --setdefaultdevhubusername)
+    fi
+    sfdx_auth_jwt_grant "${params[@]}"
+}
+
+main() {
+    verify_params
+    decrypt_key
+    authenticate_org
+}
+
+ORB_TEST_ENV="bats-core"
+if [ "${0#*"$ORB_TEST_ENV"}" == "$0" ]; then
+    main
+fi
